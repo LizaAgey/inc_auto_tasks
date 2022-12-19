4722125e60359bfd2c3420e844f191aa446d06ce
Liza Agey <agey.eliz@gmail.com>
2022-12-19 16:14:36 +0700

    ht5: fix: route links

--- src/s2-homeworks/hw05/Pages.tsx
+++ src/s2-homeworks/hw05/Pages.tsx
@@ -15,11 +15,11 @@ function Pages() {
     return (
         <div>
             <Routes>
-                {/*<Route*/}
-                {/*    path="/"*/}
-                {/*    element={<Navigate to={PATH.PRE_JUNIOR} replace />}*/}
-                {/*/>*/}
-                <Route path="/"  element={<div>Main</div>}/>
+                <Route
+                    path="/"
+                    element={<Navigate to={PATH.JUNIOR_PLUS} replace />}
+                />
+                {/*<Route path="/"  element={<div>Main</div>}/>*/}
 
                 <Route path={'*'} element={<Error404/>}/>
                 <Route path={PATH.PRE_JUNIOR} element={<PreJunior/>}/>
